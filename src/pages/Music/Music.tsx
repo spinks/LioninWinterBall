a7d724723a8d30f9f035d011700548696b9bb9ed
Ben Spinks <32001471+spinks@users.noreply.github.com>
2019-12-30 12:01:50 +0000

    Remove unused musicpopover component, and make disco link firebased
    (as the same as the dining menu link)

--- src/pages/Music/Music.tsx
+++ src/pages/Music/Music.tsx
@@ -1,28 +1,17 @@
+import React from 'react';
 import {
-  IonCard,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
   IonContent,
   IonPage,
   IonGrid,
-  IonRow,
-  IonCol,
   IonHeader,
   IonToolbar,
   IonTitle
 } from '@ionic/react';
-import React from 'react';
 
 import grid from '../../utilities/grid';
 import AppContext from '../../AppContext';
 
-// interface MusicProps {}
-
 const Music: React.FC = () => {
-  // const [showPopover, setShowPopover] = useState(false);
-  // const [popoverEvent, setPopoverEvent] = useState();
-
   const vle = Object.values(React.useContext(AppContext));
   return (
     <IonPage>
@@ -32,32 +21,8 @@ const Music: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonCard
-                class="grid-card card-white-header"
-                button
-                routerLink="/Music/disco"
-                color="light"
-              >
-                <IonCardHeader>
-                  <IonCardSubtitle>Request Songs</IonCardSubtitle>
-                  <IonCardTitle>Silent Disco</IonCardTitle>
-                </IonCardHeader>
-              </IonCard>
-            </IonCol>
-          </IonRow>
-          {grid(vle, 'music')}
-        </IonGrid>
+        <IonGrid>{grid(vle, 'music')}</IonGrid>
       </IonContent>
-      {/* <IonPopover
-        isOpen={showPopover}
-        event={popoverEvent}
-        onDidDismiss={() => setShowPopover(false)}
-      >
-        <MusicPopover dismiss={() => setShowPopover(false)} />
-      </IonPopover> */}
     </IonPage>
   );
 };
