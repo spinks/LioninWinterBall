4314af9829c5acddca13d0f0bf17289bf4b0bf21
Ben Spinks <32001471+spinks@users.noreply.github.com>
2020-04-17 10:19:06 +0100

    Make the map zoom-able and pan-able Simple npm plugin

--- src/pages/Maps/Maps.tsx
+++ src/pages/Maps/Maps.tsx
@@ -8,6 +8,8 @@ import {
 } from '@ionic/react';
 import React from 'react';
 
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
 const Maps: React.FC = () => {
   return (
     <IonPage>
@@ -17,7 +19,11 @@ const Maps: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonImg src="/assets/map/map.png" alt="map"></IonImg>
+        <TransformWrapper>
+          <TransformComponent>
+            <IonImg src="/assets/map/map.png" alt="map"></IonImg>
+          </TransformComponent>
+        </TransformWrapper>
       </IonContent>
     </IonPage>
   );
