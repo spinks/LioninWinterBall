3f265fa47d25b130f94d8a663ca35a2c0e8192b6
Ben Spinks <32001471+spinks@users.noreply.github.com>
2021-04-01 00:00:34 +0100

    Improve notification handler

--- src/utilities/notify.tsx
+++ src/utilities/notify.tsx
@@ -5,35 +5,52 @@ import { notificationsOff, notifications } from 'ionicons/icons';
 import { LocalNotifications } from '@capacitor/local-notifications';
 
 /**
+ * Checks permissions, if prompt then asks
+ * @return {Boolean} if permissions are allowed
+ */
+async function permissionCheck() {
+  const permissions = await LocalNotifications.checkPermissions().then((permission) => {
+    if (permission.display === 'prompt' || permission.display === 'prompt-with-rationale') {
+      return LocalNotifications.requestPermissions();
+    } else {
+      return permission;
+    }
+  });
+  return permissions.display === 'granted';
+}
+
+/**
  * Toggles the status of a notification, either creates notification or cancels if it exists
  * @param {NProps} notificationContent the information for the notification
- * @return {any} object with enabled (the status of that notification) and permissions (if they are allowed)
+ * @return {Boolean} enabled (the status of that notification)
  */
 async function notifyHandler(notificationContent: NProps): Promise<any> {
-  const permissions = await LocalNotifications.areEnabled();
-  if (!permissions.value) {
-    return { enabled: false, permissions: false };
-  }
   let existing = await notifyExists(notificationContent['id']);
   if (!existing) {
     notify(notificationContent);
   } else {
-    LocalNotifications.cancel({ notifications: [existing] });
+    LocalNotifications.cancel({ notifications: [{ id: notificationContent.id }] });
   }
   existing = await notifyExists(notificationContent['id']);
-  return { enabled: existing, permissions: true };
+  return existing;
 }
 
 /**
  * Check if an notification exists, either returns undefined or the notification
  * @param {number} nid notification ID
- * @return {(undefined|LocalNotificationRequest)} either undefined if it doesn't exist, or the notification id object
+ * @return {Boolean} either undefined if it doesn't exist, or the notification id object
  */
 async function notifyExists(
     nid: number
-): Promise<any> {
-  const pending = await LocalNotifications.getPending();
-  return pending['notifications'].find((e) => e['id'] === nid);
+): Promise<Boolean> {
+  const pending = await LocalNotifications.getPending().then( (res) => {
+    const ids = res.notifications.map((x) => {
+      return x['id'];
+    });
+    return ids;
+  });
+  console.log(pending);
+  return pending.includes(nid);
 }
 
 /**
@@ -81,7 +98,9 @@ const NotifyChip: React.FC<NProps> = (props) => {
   const [toastString, setToastString] = useState('');
   const [notifyOn, setNotifyOn] = useState(false);
   useEffect(() => {
-    notifyExists(props.id).then((b) => setNotifyOn(b ? true : false));
+    notifyExists(props.id).then((b) => {
+      console.log(b); setNotifyOn(b ? true : false);
+    });
   }, [props.id]);
   return (
     <React.Fragment>
@@ -95,19 +114,22 @@ const NotifyChip: React.FC<NProps> = (props) => {
           background: 'var(--ion-color-light)'
         }}
         color={notifyOn ? 'primary' : 'dark'}
-        onClick={(e) => {
+        onClick={async (e) => {
           e.stopPropagation();
-          notifyHandler(props).then((b) => {
-            setNotifyOn(b.enabled);
-            setToastString(
-              b.permissions ?
-                b.enabled ?
-                  'You will be notified' :
-                  'Notification cancelled' :
-                'Please enable notifications in settings to use this feature'
+          const permissions = await permissionCheck();
+          if (!permissions) {
+            setToastString('Please enable notifications in settings to use this feature');
+            setShowToast1(true);
+            return;
+          } else {
+            const set = await notifyHandler(props);
+            setNotifyOn(set);
+            setToastString(set ? 'You will be notified':
+                  'Notification cancelled'
             );
             setShowToast1(true);
-          });
+            return;
+          }
         }}
       >
         <IonIcon
